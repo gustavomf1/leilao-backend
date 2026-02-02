5b0b82583ebae2f93509f1326dec26df969a9949
Gustavo França <gustavo_mf1@hotmail.com>
2026-02-02 18:23:17 -0300

    Estrutura do projeto

---
+++ src/main/java/backstage/project/erpleilao/ErpleilaoApplication.java
@@ -0,0 +1,13 @@
+package backstage.project.erpleilao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpleilaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpleilaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/backstage/project/erpleilao/ErpleilaoApplicationTests.java
@@ -0,0 +1,13 @@
+package backstage.project.erpleilao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpleilaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
